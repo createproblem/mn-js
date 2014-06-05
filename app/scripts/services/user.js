e303da7fb99554a02b3eac6afd239d705ddba9ba
createproblem <createproblem@gmx.de>
2014-06-05 20:51:01 +0200

    changed token function name

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -2,11 +2,11 @@
 
 angular.module('mnjsUser', ['ngResource']).factory('UserApi', ['$resource', '$cookieStore',
   function($resource, $cookieStore) {
-    var getToken = function() {
+    var access_token = function() {
       var token = $cookieStore.get('token') || {};
       return token.access_token;
     };
 
-    return $resource('http://localhost:8000/api/me.json', {'access_token': getToken}, {
+    return $resource('http://localhost:8000/api/me.json', {'access_token': access_token}, {
     });
   }]);
