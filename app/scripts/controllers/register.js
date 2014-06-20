0c461c8da7c278c1b475a7aa114677b6a201176b
createproblem <createproblem@gmx.de>
2014-06-20 23:08:23 +0200

    added registration dummy

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -2,4 +2,14 @@
 
 angular.module('controllers.register', ['directives.ngmatch']).controller('RegisterCtrl', ['$scope',
   function($scope) {
+    $scope.register = function(isValid) {
+      if (isValid) {
+        var user = {
+          email: this.email,
+          password: this.password
+        };
+
+        $scope.registerError = true;
+      }
+    };
   }]);
