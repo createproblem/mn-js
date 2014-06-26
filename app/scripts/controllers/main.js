d8a05c94bc5441afa1d6360c7af6813e63d969e7
createproblem <createproblem@gmx.de>
2014-06-26 20:33:16 +0200

    added oauth (testing)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,16 @@
  * # MainCtrl
  * Controller of the mnJsApp
  */
-angular.module('mnJsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('mnJsApp').controller('MainCtrl', ['$scope',
+  function($scope) {
+    $scope.login = function() {
+      OAuth.initialize('LOftudjnlI49JuNEnI8P6drvYEk');
+      OAuth.popup('movie_nightmare').done(function(mn) {
+        console.log(mn);
+        mn.me().done(function(me) {
+          console.log(me.email);
+          console.log(me);
+        });
+      });
+    };
+  }]);
